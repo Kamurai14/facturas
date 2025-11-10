c35b6906e8cd92be107a6a10b4c4daa9d15148c5
Ivan <luko_260@hotmail.com>
2025-11-09 18:15:08 -0700

    Agregue el servicioControlador

--- Components/Servicios/ServicioControlador.cs
+++ Components/Servicios/ServicioControlador.cs
@@ -1,6 +1,18 @@
-﻿namespace facturas.Components.Servicios
+﻿using facturas.Components.Data;
+
+namespace facturas.Components.Servicios
 {
     public class ServicioControlador
     {
+        private readonly ServicioFacturas _servicioFacturas;
+
+        public ServicioControlador(ServicioFacturas servicioFacturas)
+        {
+            _servicioFacturas = servicioFacturas;
+        }       
+        public async Task GuardarFacturaAsync(Factura factura)
+        {
+            await _servicioFacturas.GuardarFacturaAsync(factura);
+        }
     }
 }
